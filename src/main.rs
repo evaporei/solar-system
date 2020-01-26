affb31fb001a1d9878def0816a3a97d81af25a84
Otávio Pace <otaviopp8@gmail.com>
2020-01-26 11:02:16 -0300

    main: add first OpenGL function calls

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-use glfw::{Action, Context, Key};
+use gl::types::GLuint;
+use glfw::{Action, Context, Key, OpenGlProfileHint, WindowHint};
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    let (mut window, events) = glfw.create_window(1920, 1080, "Solar System", glfw::WindowMode::Windowed)
+    glfw.window_hint(WindowHint::Samples(Some(4)));
+    glfw.window_hint(WindowHint::ContextVersionMajor(3));
+    glfw.window_hint(WindowHint::ContextVersionMinor(3));
+    glfw.window_hint(WindowHint::OpenGlForwardCompat(true));
+    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
+
+    let (mut window, events) = glfw
+        .create_window(1920, 1080, "Solar System", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
     window.set_key_polling(true);
     window.make_current();
+
+    gl::load_with(|s| window.get_proc_address(s));// kind like glew init
+
+    unsafe {
+        gl::ClearColor(0.0, 0.0, 0.0, 0.0);
+        gl::Enable(gl::DEPTH_TEST);
+        gl::DepthFunc(gl::LESS);
+    }
+
+    let mut vertex_array_id: GLuint = 0;
+
+    unsafe {
+        gl::GenVertexArrays(1, &mut vertex_array_id);
+        gl::BindVertexArray(vertex_array_id);
+    }
 
     while !window.should_close() {
         glfw.poll_events();
@@ -19,9 +42,7 @@ fn main() {
 
 fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
     match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true)
-        }
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
         _ => {}
     }
 }
